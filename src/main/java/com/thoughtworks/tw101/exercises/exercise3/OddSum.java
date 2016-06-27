0fad175d4e67604bb9f82d1711a24e19a0443065
True Sonneborn <tsonnebo@minint-v6odq0l.corporate.thoughtworks.com>
2016-06-27 14:43:11 -0400

    ex 3

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -1,8 +1,13 @@
 package com.thoughtworks.tw101.exercises.exercise3;
 
 public class OddSum {
-
+    private int count = 0;
     public int of(int start, int end) {
-        return -1;
+        for (int i = start; i <= end; i++) {
+            if (i % 2 == 1) {
+                count += i;
+            }
+        }
+        return count;
     }
 }
